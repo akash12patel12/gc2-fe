130327202afe4626d294a691448fa2a4574d4a9f
akash12patel12 <akash12patel12@gmail.com>
2023-07-19 19:15:22 +0530

    front end for login and sign up

---
+++ index.js
@@ -0,0 +1,50 @@
+const apiurl  = "http://localhost:3000"
+async function register(e){
+    e.preventDefault();
+  axios.post(`${apiurl}/register`, {
+    "name" : e.target.name.value,
+    "email" : e.target.email.value,
+    "phone" :e.target.phone.value,
+    "password" : e.target.password.value
+   }).then(res=>{
+     alert("User Created");
+   }).catch(err=>{
+    alert(err.response.data.errorMsg);
+   })
+}
+
+function enablelogin(e){
+  e.preventDefault();
+ document.getElementById('login-form').style.display = 'block';
+ document.getElementById('register-form').style.display = 'none';
+
+}
+
+function enableregister(e){
+  e.preventDefault();
+  document.getElementById('register-form').style.display = 'block'
+  document.getElementById('login-form').style.display = 'none'
+
+}
+
+function login(e){
+  e.preventDefault();
+  axios.post(`${apiurl}/login`, {"email" :e.target.email.value, "password" : e.target.password.value}).then(res=>{
+    alert('Logged In Successfully');
+    const token = res.data.token;
+    localStorage.setItem("token", token);
+    window.location.href = "chat.html";
+  }).catch(err=>{
+    // console.log();
+    if(err.response.status === 401){
+      alert('Password Is Incorrect!');
+    }
+    else if(err.response.status === 404){
+      alert('No User Found!');
+    }
+    else {
+      alert('Some Error Occured!');
+
+    }
+  });
+}
