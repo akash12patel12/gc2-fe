7ae151c1b2a509d0d4ea5785745452fbbdcb69e4
akash12patel12 <akash12patel12@gmail.com>
2023-07-27 18:42:16 +0530

    auto reload i.e. realtime

--- chat.js
+++ chat.js
@@ -7,15 +7,46 @@ const config = {
   };
 
 
-checkLogin();
-function checkLogin(){
-  if(!localStorage.getItem('token')){
-    window.location.href = "index.html";
-  }
+// checkLogin();
+// function checkLogin(){
+//   if(!localStorage.getItem('token')){
+//     window.location.href = "index.html";
+//   }
+// }
+
+// function logOut(e) {
+//     e.preventDefault();
+//     localStorage.clear();
+//     window.location.href = "login.html";
+// }
+
+function sendm(e){
+    e.preventDefault();
+    // console.log("called");
+    axios.post(`${url}/send`, { message : e.target.message.value}, config).then(data=>{
+      location.reload();
+    });
+    
 }
 
-function logOut(e) {
-    e.preventDefault();
-    localStorage.clear();
-    window.location.href = "login.html";
+function getall(){
+    axios.get(`${url}/getall`, config).then(async messages=>{
+      const chatarea = document.getElementById('chat-area');
+      chatarea.innerHTML = "";
+      await messages.data.forEach( msg => {
+        chatarea.innerHTML = chatarea.innerHTML + `${msg.sender} : ${msg.message} <br>`
+       });
+
+       var objDiv = document.getElementById("chat-area");
+       objDiv.scrollTop = objDiv.scrollHeight;
+    }).catch(err=>{
+      console.log(err);
+      alert('Something Went Wrong');
+    });
 }
+
+// getall();
+
+
+// setTimeInterval(getall(), 1000);
+setInterval(function() {getall();}, 1000);
